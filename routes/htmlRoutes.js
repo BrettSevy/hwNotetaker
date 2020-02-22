479ea62c0252d9d7a029876f44f11af2976894ed
Brett Sevy <justbrettt@gmail.com>
2020-02-21 21:49:18 -0600

    able to delete items updated read me

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,18 +3,13 @@ const path = require("path");
 
 module.exports = function (app) {
 
-//   // html for home page
-//   // app.get("/", function (req, res) {
-//   //   res.sendFile(path.join(__dirname, "index.html"));
-//   // });
-
-  // html for note taking page
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-
-  
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 };
 
--- server.js
+++ server.js
@@ -6,22 +6,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
 
 require("./routes/apiRoutes")(app)
 require("./routes/htmlRoutes")(app)
 
-// app.get("/api/notes", function (req, res) {
-//     res.sendFile(path.join(__filename, "../db/db.json"));
-// });
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// listener
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
